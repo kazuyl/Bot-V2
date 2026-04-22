075e8c97cd39091ea92adb449d428849a32b007a
kazuyl <huytranvn4@gmail.com>
2026-04-22 21:41:30 +0200

    Update test_price_update.py

--- tests/test_price_update.py
+++ tests/test_price_update.py
@@ -1,8 +1,12 @@
 import requests
 
-url = 'http://localhost:5000/price_update'
-payload = {'price': 27040}
+url = "https://bot-v2-production.up.railway.app/price_update"
+
+payload = {
+    "price": 27040
+}
 
 r = requests.post(url, json=payload)
+
 print(r.status_code)
 print(r.text)
