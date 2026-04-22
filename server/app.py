67ff8a96b3cb3f2125d44692320a4f4b35c139e6
kazuyl <huytranvn4@gmail.com>
2026-04-22 21:19:06 +0200

    Update app.py

--- server/app.py
+++ server/app.py
@@ -316,3 +316,9 @@ def load_state() -> None:
 if __name__ == '__main__':
     load_state()
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
